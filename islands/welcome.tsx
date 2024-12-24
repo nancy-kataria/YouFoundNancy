91a5a81151b075fabdfcd5d1e67e526b523077b7
nancy-kataria <katarianancy8@gmail.com>
2024-12-23 23:22:38 -0800

    enabled modal use

--- islands/welcome.tsx
+++ islands/welcome.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "https://esm.sh/v128/preact@10.22.0/hooks";
+import { useEffect, useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
+import Fact from "./Fact.tsx";
 
 export default function Welcome() {
+  const [openFact, setOpenFact] = useState(false)
+
   useEffect(() => {
     const typingEffect = () => {
       let index = 0;
@@ -27,6 +30,8 @@ export default function Welcome() {
   return (
     <div>
       <p id="typing"></p>
+      <p onClick={()=>setOpenFact(true)}>[Interesting fact]</p>
+      {openFact && <Fact openFact={openFact} />}
     </div>
   );
 }
