ebb38c51e5090ab0b09460d8246c5c1786e0eb46
nancy-kataria <katarianancy8@gmail.com>
2024-12-23 22:31:54 -0800

    added fact modal

---
+++ islands/Fact.tsx
@@ -0,0 +1,11 @@
+function Fact() {
+  return (
+    <div class="modal">
+        <h4>What do I google the most?</h4>
+        <p>How to center a div?
+        </p>
+    </div>
+  )
+}
+
+export default Fact
