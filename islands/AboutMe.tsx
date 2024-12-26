a312ed7a7f51f2c9764478e55a67c6d367ce8939
nancy-kataria <katarianancy8@gmail.com>
2024-12-25 23:50:03 -0800

    moved a component

--- islands/AboutMe.tsx
+++ islands/AboutMe.tsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
+import { useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
 import Fact from "./Fact.tsx";
 
 function AboutMe() {
   const [openFact, setOpenFact] = useState<boolean>(false);
 
-  useEffect(() => {
-    const typingEffect = () => {
-      let index = 0;
-      const speed = 40;
-      const text = "Hello World!";
-
-      const element = document.getElementById("typing");
-      if (!element) return;
-
-      const typeCharacter = () => {
-        if (index < text.length) {
-          element.textContent += text.charAt(index);
-          index += 1;
-          setTimeout(typeCharacter, speed);
-        }
-      };
-
-      typeCharacter();
-    };
-
-    typingEffect();
-  }, []);
-
   return (
     <div>
-      <p id="typing"></p>
       <p onClick={() => setOpenFact(true)}>[Interesting fact]</p>
       {openFact && <Fact openFact={openFact} />}
     </div>
