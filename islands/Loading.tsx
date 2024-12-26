e52235486f3c35b54b41fba0cd7cdd6769d40289
nancy-kataria <katarianancy8@gmail.com>
2024-12-25 23:49:48 -0800

    added set time outs

--- islands/Loading.tsx
+++ islands/Loading.tsx
@@ -1,9 +1,50 @@
+import { useEffect } from "https://esm.sh/v128/preact@10.22.0/hooks";
+
 function Loading() {
+  useEffect(() => {
+    const weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const d = new Date();
+    let day = weekday[d.getDay()];
+
+    const textArray = ["Hello World!", `Happy ${day}`];
+    const typingEffect = (text: string, position: number) => {
+      let index = 0;
+      const speed = 40;
+
+      const element = document.getElementById(`typing-${position}`);
+      if (!element) return;
+
+      const typeCharacter = () => {
+        if (index < text.length) {
+          element.textContent += text.charAt(index);
+          index += 1;
+          setTimeout(typeCharacter, speed);
+        }
+      };
+
+      typeCharacter();
+    };
+
+    typingEffect(textArray[0], 0);
+    setTimeout(() => {
+      typingEffect(textArray[1], 1);
+    }, 1000);
+  }, []);
+
   return (
-    <div>
-        <p>Loading...</p>
+    <div class="modal">
+      <p id="typing-0"></p>
+      <p id="typing-1"></p>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
