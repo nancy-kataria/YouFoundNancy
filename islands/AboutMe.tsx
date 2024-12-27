57453e325985dfaeb7b1ec20b0f65a6ad65f9663
nancy-kataria <katarianancy8@gmail.com>
2024-12-26 20:23:39 -0800

    added name banner

--- islands/AboutMe.tsx
+++ islands/AboutMe.tsx
@@ -1,11 +1,38 @@
-import { useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
+import { useEffect, useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
 import Fact from "./Fact.tsx";
 
 function AboutMe() {
   const [openFact, setOpenFact] = useState<boolean>(false);
 
+  const name = [
+    " _   _                          _   __      _             _       ",
+    "| \\ | |                        | | / /     | |           (_)",
+    "|  \\| | __ _ _ __   ___ _   _  | |/ /  __ _| |_ __ _ _ __ _  __ _ ",
+    "| . ` |/ _` | '_ \\ / __| | | | |    \\ / _` | __/ _` | '__| |/ _` |",
+    "| |\\  | (_| | | | | (__| |_| | | |\\  \\ (_| | || (_| | |  | | (_| |",
+    "\\_| \\_/\\__,_|_| |_|\\___|\\__, | \\_| \\_/\\__,_|\\__\\__,_|_|  |_|\\__,_|",
+    "                         __/ |                                    ",
+    "                        |___/                                     ",
+  ];
+
+  useEffect(() => {
+    const addingChild = (index: number) => {
+      let element = document.getElementById("banner");
+      let childElement = document.createElement("pre");
+      childElement.id=`name-${index}`
+
+      element?.appendChild(childElement)
+    }
+
+    for(let index=0; index<name.length;index++){
+      addingChild(index)
+    }
+  }, []);
+
   return (
     <div>
+      <div id="banner">
+      </div>
       <p onClick={() => setOpenFact(true)}>[Interesting fact]</p>
       {openFact && <Fact openFact={openFact} />}
     </div>
