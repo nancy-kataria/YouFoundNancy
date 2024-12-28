1ed49fa4a220a608a6de2db8a24424cc9b9947a5
nancy-kataria <katarianancy8@gmail.com>
2024-12-27 20:15:56 -0800

    added loading bar percentage

--- islands/Loading.tsx
+++ islands/Loading.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "https://esm.sh/v128/preact@10.22.0/hooks";
+import { useEffect, useState } from "https://esm.sh/v128/preact@10.22.0/hooks";
 
 function Loading() {
+  const [percentage, setPercentage] = useState<number>(0)
   useEffect(() => {
     const weekday = [
       "Sunday",
@@ -37,12 +38,37 @@ function Loading() {
     setTimeout(() => {
       typingEffect(textArray[1], 1);
     }, 1000);
+
+    setTimeout(() => {
+      const character = "🁢"
+      let count = 0;
+      const maxCount = 20;
+      let percent = 0;
+
+      const lineElement = document.getElementById(`loading-bar`);
+      
+      if (lineElement) {
+        lineElement.textContent += "Loading: ";
+        const interval = setInterval(() => {
+          if (count >= maxCount) {
+            clearInterval(interval);
+          } else {
+            lineElement.textContent += character;
+            percent+=5
+            setPercentage(percent)
+            count++;
+          }
+        }, 20);
+      }
+    }, 2000);
   }, []);
 
   return (
     <div class="modal">
       <p id="typing-0"></p>
       <p id="typing-1"></p>
+      <p id="loading-bar"></p>
+      <p>{percentage} %</p>
     </div>
   );
 }
