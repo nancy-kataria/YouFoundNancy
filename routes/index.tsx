1f9b578b03931a1ba31b6ff856c079d691fa1fae
nancy-kataria <katarianancy8@gmail.com>
2024-12-28 16:19:59 -0800

    centered div

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ import Welcome from "../islands/welcome.tsx";
 
 export default function Home() {
   return (
-    <div class="main-body">
+    <div class="main-body flex-center">
       <Welcome />
     </div>
   );
