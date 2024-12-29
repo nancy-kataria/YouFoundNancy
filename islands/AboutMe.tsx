80d7081cb8321b62bbced8c5517e79592d24ce17
nancy-kataria <katarianancy8@gmail.com>
2024-12-28 20:29:16 -0800

    moved functions out of useEffect

--- islands/AboutMe.tsx
+++ islands/AboutMe.tsx
@@ -15,40 +15,44 @@ function AboutMe() {
     "                        |___/                                     ",
   ];
 
-  useEffect(() => {
-    const addingChild = (index: number) => {
-      let element = document.getElementById("banner");
-      let childElement = document.createElement("pre");
-      childElement.id = `name-${index}`;
+  const description =
+    "A full stack web developer and a graduate student pursuing a Master's in Computer Science at CSUF. I work with MERN stack and have recently expanded my skill set by learning Confluent Kafka to build real-time, event-driven applications.";
 
-      element?.appendChild(childElement);
+  const addingChild = (index: number) => {
+    let element = document.getElementById("banner");
+    let childElement = document.createElement("pre");
+    childElement.id = `name-${index}`;
+
+    element?.appendChild(childElement);
+  };
+
+  const typingEffect = (text: string, position: number) => {
+    let index = 0;
+    const speed = 20;
+
+    const element = document.getElementById(`name-${position}`);
+    if (!element) return;
+
+    const typeCharacter = () => {
+      if (index < text.length) {
+        element.textContent += text.charAt(index);
+        index += 1;
+        setTimeout(typeCharacter, speed);
+      }
     };
 
+    typeCharacter();
+  };
+
+  useEffect(() => {
     for (let index = 0; index < name.length; index++) {
       addingChild(index);
     }
-
-    const typingEffect = (text: string, position: number) => {
-      let index = 0;
-      const speed = 20;
-
-      const element = document.getElementById(`name-${position}`);
-      if (!element) return;
-
-      const typeCharacter = () => {
-        if (index < text.length) {
-          element.textContent += text.charAt(index);
-          index += 1;
-          setTimeout(typeCharacter, speed);
-        }
-      };
-
-      typeCharacter();
-    };
-
     for (let index = 0; index < name.length; index++) {
       typingEffect(name[index], index);
     }
+
+    // setTimeout(()=> {typingEffect(description)}, 2000)
   }, []);
 
   return (
--- islands/Loading.tsx
+++ islands/Loading.tsx
@@ -1,6 +1,59 @@
 import { useEffect } from "https://esm.sh/v128/preact@10.22.0/hooks";
 
 function Loading() {
+  const textArray = ["Hello, World!" // `Happy ${day}`
+  ];
+
+  const typingEffect = (text: string, position: number) => {
+    let index = 0;
+    const speed = 40;
+
+    const element = document.getElementById(`typing-${position}`);
+    if (!element) return;
+
+    const typeCharacter = () => {
+      if (index < text.length) {
+        element.textContent += text.charAt(index);
+        index += 1;
+        setTimeout(typeCharacter, speed);
+      }
+    };
+
+    typeCharacter();
+  };
+
+  const renderLoadingBar = () => {
+    const character = "🁢";
+    let count = 0;
+    // Maximum 20 bars
+    const maxCount = 20;
+    let percent = 0;
+
+    const lineElement = document.getElementById(`loading-bar`);
+    const percentElement = document.getElementById(`percentage`);
+
+    if (lineElement) {
+      lineElement.textContent += "Loading: ";
+
+      // In interval of 0.02 seconds, every loading bar is rendered
+      const interval = setInterval(() => {
+        if (count >= maxCount) {
+          // clear the interval when count goes above 20
+          clearInterval(interval);
+        } else {
+          lineElement.textContent += character;
+
+          // Incrementing the percent by 5 to complete the loading in 20 bars
+          percent += 5;
+          if (percentElement) {
+            percentElement.textContent = `${percent} %`;
+          }
+          count++;
+        }
+      }, 20);
+    }
+  };
+
   useEffect(() => {
     // Calculating Weekday
     // const weekday = [
@@ -15,27 +68,6 @@ function Loading() {
     // const d = new Date();
     // let day = weekday[d.getDay()];
 
-    const textArray = ["Hello, World!" // `Happy ${day}`
-    ];
-
-    const typingEffect = (text: string, position: number) => {
-      let index = 0;
-      const speed = 40;
-
-      const element = document.getElementById(`typing-${position}`);
-      if (!element) return;
-
-      const typeCharacter = () => {
-        if (index < text.length) {
-          element.textContent += text.charAt(index);
-          index += 1;
-          setTimeout(typeCharacter, speed);
-        }
-      };
-
-      typeCharacter();
-    };
-
     typingEffect(textArray[0], 0);
     // setTimeout(() => {
     //   typingEffect(textArray[1], 1);
@@ -43,35 +75,7 @@ function Loading() {
 
     // Rendering the loading bars
     setTimeout(() => {
-      const character = "🁢";
-      let count = 0;
-      // Maximum 20 bars
-      const maxCount = 20;
-      let percent = 0;
-
-      const lineElement = document.getElementById(`loading-bar`);
-      const percentElement = document.getElementById(`percentage`);
-
-      if (lineElement) {
-        lineElement.textContent += "Loading: ";
-
-        // In interval of 0.02 seconds, every loading bar is rendered
-        const interval = setInterval(() => {
-          if (count >= maxCount) {
-            // clear the interval when count goes above 20
-            clearInterval(interval);
-          } else {
-            lineElement.textContent += character;
-
-            // Incrementing the percent by 5 to complete the loading in 20 bars
-            percent += 5;
-            if (percentElement) {
-              percentElement.textContent = `${percent} %`;
-            }
-            count++;
-          }
-        }, 20);
-      }
+      renderLoadingBar();
     }, 1000);
   }, []);
 
