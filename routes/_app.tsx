450248bd0d3be7a401bedd0f730055d3ef28d383
nancy-kataria <katarianancy8@gmail.com>
2025-03-04 12:53:15 -0800

    added gtag to the head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,16 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Nancy Kataria</title>
         <link rel="stylesheet" href="/styles.css" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHZ5B939S8"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VHZ5B939S8');
+          `
+        }} />
       </head>
       <body>
         <Component />
